8a2c0678ebf1347a54b0d935425dd3b093e54c0d
mrudula-11 <96371740+mrudula-11@users.noreply.github.com>
2022-01-20 11:17:31 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,10 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-@RestController
+
 public class App 
 {
-    @GetMapping("/hello")
+   
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
